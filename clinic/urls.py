94e252250c3621e600751c4bd37eee1402f18685
Svetlana Novikova <seaciliya2012@yandex.ru>
2024-08-08 21:51:20 +0300

    "gw 4"

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,6 +1,10 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
 from clinic.apps import ClinicConfig
 from users.apps import UsersConfig
 
 app_name = ClinicConfig.name
 urlpatterns = [
+    path("", TemplateView.as_view(template_name='clinic/index.html'), name='home'),
     ]
